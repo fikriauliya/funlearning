0d9018da91bb5f18daf7cbc1e7ecfb39e6fac006
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2022-11-04 06:16:43 +0700

    feat(gameoflife): draw base and grid

---
+++ gameoflife/main.ts
@@ -0,0 +1,70 @@
+const SIZE = 20;
+const N = 30;
+
+let grid: boolean[][];
+
+const initGrid = () => {
+  //nxn boolean grid
+  grid = new Array(N);
+  for (let i = 0; i < N; i++) {
+    grid[i] = new Array(N);
+    for (let j = 0; j < N; j++) {
+      grid[i][j] = false;
+    }
+  }
+};
+
+const drawBase = () => {
+  let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  let ctx = canvas.getContext("2d");
+  if (ctx) {
+    ctx.strokeStyle = "grey";
+    ctx.lineWidth = 0.5;
+    //create n x n rectangle with size SIZE without fill
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        ctx.rect(i * SIZE, j * SIZE, SIZE, SIZE);
+      }
+    }
+    ctx.stroke();
+  }
+};
+
+function drawGrid() {
+  let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  let ctx = canvas.getContext("2d");
+  if (ctx) {
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        if (grid[i][j]) {
+          ctx.fillStyle = "black";
+          ctx.fillRect(i * SIZE, j * SIZE, SIZE, SIZE);
+        }
+      }
+    }
+  }
+}
+
+const handleCanvasClick = () => {
+  let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  canvas.addEventListener("click", (e) => {
+    //get position of click, snap to grid
+    let x = Math.floor(e.offsetX / SIZE);
+    let y = Math.floor(e.offsetY / SIZE);
+    grid[x][y] = !grid[x][y];
+    drawGrid();
+  });
+};
+
+const handleStart = () => {
+  let startButton = document.getElementById("start") as HTMLButtonElement;
+  startButton.addEventListener("click", () => {});
+};
+
+//draw rectangle in canvas on document load
+window.onload = () => {
+  initGrid();
+  drawBase();
+  handleCanvasClick();
+  handleStart();
+};
